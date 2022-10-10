9e72a6f5f02f406491e08153dfa38046bf8f8c3b
meenakshi gupta <guptamunmun45@gmail.com>
2022-10-10 22:16:20 +0530

    create user api

--- src/validators/validation.js
+++ src/validators/validation.js
@@ -1,22 +1,53 @@
-const userModel = require("../model/userModel")
+const mongoose = require('mongoose')
 
 
 
 const isValid = function (value) {
-    if (typeof value == "undefined" || value == null) return false;
-    if (typeof value == "string" && value.trim().length > 0) return true;
+    
+    if (typeof value === "undefined" || typeof value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
 };
 
-const isValidRequestBody = function (object) {
-    return Object.keys(object).length > 0;
-};
+const nameFormet= function(value){
+    const regex=/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/g
+    if(!regex.test(value)) return false
+    return true
+}
 
-const isValidEmail = function (email) {
-    const regexForEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-    return regexForEmail.test(email);
-};
+const isValidNumber = function (value) {
+    const noNumber = /^(\+91[\-\s]?)?[0]?(91)?[6-9]\d{9}$/g
+    if (typeof value !== 'string') return false
+    if (noNumber.test(value) === false) return false
+    return true
+}
 
-const isValidPhone = function (phone) {
-    const regexForMobile = /^[6-9]\d{9}$/;
-    return regexForMobile.test(phone);
-};
+
+const isValidEmail = function (value) {
+    let mailFormat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
+    if (!(value.match(mailFormat))) return false
+    return true
+
+}
+
+const isValidRequestBody = function (requestbody) {
+    return Object.keys(requestbody).length > 0;
+}
+
+const isValidObjectId = function (ObjectId) {
+    return mongoose.Types.ObjectId.isValid(ObjectId)
+}
+
+const isValidPincode = function (value) {
+    let pinCodeValidation = /^[0-9]{6}$/;
+    if (!(pinCodeValidation.test(value))) return false
+    return true
+}
+
+const isValidPassword=function(value){
+    let passwordPattern = /^[a-zA-Z0-9!@#$%&*]{8,15}$/;
+        if (!(passwordPattern.test(value))) return false
+        return true
+}
+
+module.exports = { isValid, isValidEmail, isValidNumber, isValidRequestBody, isValidObjectId, isValidPincode,isValidPassword,nameFormet }
